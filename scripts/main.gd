de18bc3d970e40b772576c73ab0c8feb7b0e6faf
BlackDragon <83983652+TheBlackDragon4@users.noreply.github.com>
2024-11-16 16:36:15 +0100

    Fix bug and add livebar modifications

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+@onready var hearts_container = $ui_layer/lifebar
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hearts_container.set_max_hearts(7)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
