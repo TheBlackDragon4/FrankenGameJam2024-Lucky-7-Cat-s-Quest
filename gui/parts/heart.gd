5b93d53e6b026f9e30240fd42c11c0245736a0f3
NicoTheCode <84142876+NicoTheCode@users.noreply.github.com>
2024-11-16 13:58:32 +0100

    Basic healthbar

---
+++ gui/parts/heart.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+@onready var sprite = $heart
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite.play("full")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# State:
+# 0: Empty
+# 1: Full
+func update(animation: String):
+	sprite.play(animation)
