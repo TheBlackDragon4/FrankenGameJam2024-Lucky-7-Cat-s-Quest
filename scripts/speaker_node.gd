c9818d6f54de212c8ce6cead4194339be61f7292
David Duschek <149827508+davemaster1203@users.noreply.github.com>
2024-11-17 16:23:26 +0100

    implemented and tested speaker nodes
    implemented and tested speaker nodes

--- scripts/speaker_node.gd
+++ scripts/speaker_node.gd
@@ -1,14 +1,30 @@
 extends Node2D
 
 @export var text: String = ""
-@export var keybind: String = ""
+#@export var keybind: String = ""
+@onready var textBubble = $"%Text"
+@onready var bubble = $"%textbubble"
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	textBubble.text = "[color=black]" + text + "[/color]"
+	#interaction_area.interact = Callable(self, "_on_interact")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_body_entered(body: Node2D) -> void:
+	textBubble.visible = not textBubble.visible
+	bubble.visible = not bubble.visible
+
+
+func _on_body_exited(body: Node2D) -> void:
+	textBubble.visible = not textBubble.visible
+	bubble.visible = not bubble.visible
